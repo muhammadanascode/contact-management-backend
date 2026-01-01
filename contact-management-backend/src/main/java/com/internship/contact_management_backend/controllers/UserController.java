dda02684ee2654ecd43bbf42098f5071065e6527
muhammadanascode <anassohal34343@gmail.com>
2026-01-01 19:44:08 +0500

    feat: Created User controller to serve requests

---
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/controllers/UserController.java
@@ -0,0 +1,22 @@
+package com.internship.contact_management_backend.controllers;
+
+import com.internship.contact_management_backend.entities.User;
+import com.internship.contact_management_backend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User savedUser = userService.register(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+}
