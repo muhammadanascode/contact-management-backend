f88f87addd91a7d2245506a9d3af9e5c63b716d5
muhammadanascode <anassohal34343@gmail.com>
2026-01-01 18:42:30 +0500

    feat: Created a service folder and added User service file

---
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/services/UserService.java
@@ -0,0 +1,35 @@
+package com.internship.contact_management_backend.services;
+
+import com.internship.contact_management_backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.internship.contact_management_backend.entities.User;
+
+@Service
+public class UserService {
+
+        @Autowired
+        private UserRepository userRepository;
+
+        @Autowired
+        private PasswordEncoder passwordEncoder;
+
+        /**
+         * Registers a new user
+         */
+        public User register(User user) {
+
+            // 1. Validate business rule: email must be unique
+            if (userRepository.existsByEmail(user.getEmail())) {
+                throw new IllegalArgumentException("Email already exists");
+            }
+
+            // 2. Hash password before saving
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+            // 4. Save user
+            return userRepository.save(user);
+        }
+    }
