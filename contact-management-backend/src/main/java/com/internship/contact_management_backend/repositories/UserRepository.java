6087ec1f1918ed0bf54f32c83fc4dc4fafa3e422
muhammadanascode <anassohal34343@gmail.com>
2026-01-01 18:42:02 +0500

    feat: Added two methods in repository

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/repositories/UserRepository.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/repositories/UserRepository.java
@@ -3,6 +3,14 @@ package com.internship.contact_management_backend.repositories;
 import com.internship.contact_management_backend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    // Find user by email
+    Optional<User> findByEmail(String email);
+
+    // Check if email already exists
+    boolean existsByEmail(String email);
 
 }
