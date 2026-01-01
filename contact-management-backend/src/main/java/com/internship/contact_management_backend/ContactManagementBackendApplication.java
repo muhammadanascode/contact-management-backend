d87856b4ef0214e69e7eca07279ca27e70399b32
muhammadanascode <anassohal34343@gmail.com>
2026-01-01 15:28:50 +0500

    Initial Commit - Spring boot project setup

---
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/ContactManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.internship.contact_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagementBackendApplication.class, args);
+	}
+
+}
---
+++ contact-management-backend/src/test/java/com/internship/contact_management_backend/ContactManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.contact_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
