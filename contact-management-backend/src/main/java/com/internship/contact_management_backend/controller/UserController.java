5b38c269100dbc1f7604433a5547616fe26fc427
muhammadanascode <anassohal34343@gmail.com>
2026-01-03 19:33:56 +0500

    fix: added @getter annotation to fix the serialization error

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/controller/UserController.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.internship.contact_management_backend.controller;
 import com.internship.contact_management_backend.dto.UserRegisterDto;
 import com.internship.contact_management_backend.entity.User;
 import com.internship.contact_management_backend.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserRegisterDto> register(@RequestBody UserRegisterDto user) {
+    public ResponseEntity<UserRegisterDto> register(@Valid @RequestBody UserRegisterDto user) {
         User savedUser = userService.register(user.toEntity());
         return ResponseEntity.ok(savedUser.toDto());
     }
