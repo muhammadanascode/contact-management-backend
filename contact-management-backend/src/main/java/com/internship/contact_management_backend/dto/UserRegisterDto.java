0177d0aa474650f33a7fccba5e5812cf423807fa
muhammadanascode <anassohal34343@gmail.com>
2026-01-03 14:28:43 +0500

    feat: Created dto to register new user

---
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/dto/UserRegisterDto.java
@@ -0,0 +1,11 @@
+package com.internship.contact_management_backend.dto;
+
+public class UserRegisterDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    //TODO: write toEntity function
+
+}
