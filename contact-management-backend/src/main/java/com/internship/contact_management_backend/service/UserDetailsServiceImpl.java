4f9913244b1188c29809c22819f4a4c987cfc6b9
muhammadanascode <anassohal34343@gmail.com>
2026-01-04 21:50:03 +0500

    feat: Implemented UserDetailsService for authentication

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/service/UserDetailsServiceImpl.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/service/UserDetailsServiceImpl.java
@@ -1,2 +1,29 @@
-package com.internship.contact_management_backend.service;public class UserDetailsServiceImpl {
+package com.internship.contact_management_backend.service;
+
+import com.internship.contact_management_backend.entity.User;
+import com.internship.contact_management_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user != null) {
+            return org.springframework.security.core.userdetails.User.builder()
+                                                                     .username(user.getEmail())
+                                                                     .password(user.getPassword())
+                                                                     .build();
+        }
+        throw new UsernameNotFoundException("User not found with username: " + email);
+    }
 }
