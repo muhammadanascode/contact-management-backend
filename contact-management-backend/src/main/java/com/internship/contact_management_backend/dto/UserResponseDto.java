4704bee72b09728eadd52cee980101ea727fee5a
muhammadanascode <anassohal34343@gmail.com>
2026-01-04 18:49:25 +0500

    feat: Added a method to convert entity into response dto

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/dto/UserResponseDto.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/dto/UserResponseDto.java
@@ -1,2 +1,21 @@
-package com.internship.contact_management_backend.dto;public class UserResponseDto {
+package com.internship.contact_management_backend.dto;
+
+import com.internship.contact_management_backend.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class UserResponseDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public static UserResponseDto from(User user) {
+        return UserResponseDto.builder()
+                              .firstName(user.getFirstName())
+                              .lastName(user.getLastName())
+                              .email(user.getEmail())
+                              .build();
+    }
 }
