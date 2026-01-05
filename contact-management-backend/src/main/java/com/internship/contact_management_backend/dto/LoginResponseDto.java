c6803f431f15d648097db36ec9a5f0a65aa5c883
muhammadanascode <anassohal34343@gmail.com>
2026-01-05 22:14:46 +0500

    feat: Created response dto ro send token

---
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/dto/LoginResponseDto.java
@@ -0,0 +1,10 @@
+package com.internship.contact_management_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponseDto {
+    private String token;
+}
