a8975aa7c5f304bb72f37e6f070788d8a5c24369
muhammadanascode <anassohal34343@gmail.com>
2026-01-05 21:58:25 +0500

    feat: Added login point

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/controller/UserController.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.internship.contact_management_backend.controller;
 
+import com.internship.contact_management_backend.dto.UserLoginDto;
 import com.internship.contact_management_backend.dto.UserRegisterDto;
 import com.internship.contact_management_backend.dto.UserResponseDto;
 import com.internship.contact_management_backend.entity.User;
+import com.internship.contact_management_backend.service.UserDetailsServiceImpl;
 import com.internship.contact_management_backend.service.UserService;
+import com.internship.contact_management_backend.utils.JwtUtil;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,10 +22,29 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping("/register")
     public ResponseEntity<UserResponseDto> register(@Valid @RequestBody UserRegisterDto user) {
         User savedUser = userService.register(user.toEntity());
         return ResponseEntity.ok(savedUser.toDto());
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid @RequestBody UserLoginDto user){
+      authenticationManager.authenticate(
+              new UsernamePasswordAuthenticationToken(user.getEmail(),user.getPassword()));
+        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getEmail());
+        String token = jwtUtil.generateToken(
+                userDetails.getUsername());
+        return token;
     }
 
 }
