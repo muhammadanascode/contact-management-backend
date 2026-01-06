6332b95fab3a6ae9de25f1d3454fa1915159c176
muhammadanascode <anassohal34343@gmail.com>
2026-01-06 23:09:04 +0500

    feat: Created a contact entity

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/entity/Contact.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/entity/Contact.java
@@ -1,2 +1,35 @@
-package com.internship.contact_management_backend.entity;public class Contact {
+package com.internship.contact_management_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 20)
+    private String phoneNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
