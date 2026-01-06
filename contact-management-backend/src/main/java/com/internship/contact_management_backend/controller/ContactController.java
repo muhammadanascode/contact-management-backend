c8322b4d41d718d3c5bd4d2b6e37bcbe51c5ade4
muhammadanascode <anassohal34343@gmail.com>
2026-01-06 10:03:37 +0500

    feat: Created a controller to serve requests related to contacts

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/controller/ContactController.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/controller/ContactController.java
@@ -1,2 +1,16 @@
-package com.internship.contact_management_backend.controller;public class ContactController {
+package com.internship.contact_management_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactController {
+
+    @GetMapping
+    public String getContacts() {
+        return "List of contacts";
+    }
 }
