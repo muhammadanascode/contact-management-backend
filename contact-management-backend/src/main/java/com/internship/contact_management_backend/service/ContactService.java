3abf83cf9f994cd4db89c2c2d172d2174a5eb363
muhammadanascode <anassohal34343@gmail.com>
2026-01-06 23:47:36 +0500

    feat: created contact service createContact and getContactByUserId method

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/service/ContactService.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/service/ContactService.java
@@ -1,2 +1,23 @@
-package com.internship.contact_management_backend.service;public class ContactService {
+package com.internship.contact_management_backend.service;
+
+import com.internship.contact_management_backend.entity.Contact;
+import com.internship.contact_management_backend.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    public Contact createContact(Contact contact){
+        return contactRepository.save(contact);
+    }
+
+    public List<Contact> getContactsByUserId(Long userId){
+        return contactRepository.findByUserId(userId);
+    }
 }
