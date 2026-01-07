17c77ef72c0b72fbade4a0f2fbe2b9e1735d4769
muhammadanascode <anassohal34343@gmail.com>
2026-01-07 23:52:00 +0500

    feat: created a method to delete contact

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/service/ContactService.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/service/ContactService.java
@@ -5,6 +5,7 @@ import com.internship.contact_management_backend.entity.User;
 import com.internship.contact_management_backend.repository.ContactRepository;
 import com.internship.contact_management_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -41,4 +42,24 @@ public class ContactService {
         // fetch contacts by user id
         return contactRepository.findByUserId(user.getId());
     }
+
+    public void deleteContact(Long contactId, String email) {
+        // find user by email
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        // find contact by id
+        Contact contact = contactRepository.findContactById(contactId);
+        if (contact == null) {
+            throw new IllegalArgumentException("Contact not found with id: " + contactId);
+        }
+        // check if the contact belongs to the user
+        if (!contact.getUser().getId().equals(user.getId())) {
+            throw new BadCredentialsException("Bad Credentials");
+        }
+        // delete the contact
+        contactRepository.delete(contact);
+    }
+
 }
