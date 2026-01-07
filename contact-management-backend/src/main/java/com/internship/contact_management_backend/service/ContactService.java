9658c40c7f9f04fe80d10e204a3225a20a37d363
muhammadanascode <anassohal34343@gmail.com>
2026-01-07 22:43:20 +0500

    feat: created method to register new contact

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/service/ContactService.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/service/ContactService.java
@@ -1,10 +1,12 @@
 package com.internship.contact_management_backend.service;
 
 import com.internship.contact_management_backend.entity.Contact;
+import com.internship.contact_management_backend.entity.User;
 import com.internship.contact_management_backend.repository.ContactRepository;
+import com.internship.contact_management_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -13,9 +15,22 @@ public class ContactService {
     @Autowired
     private ContactRepository contactRepository;
 
-    public Contact createContact(Contact contact){
+    @Autowired
+    private UserRepository userRepository;
+
+    public Contact createContact(Contact contact, String email) {
+
+        // find user by email
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        // attach contact to user
+        contact.setUser(user);
+
         return contactRepository.save(contact);
     }
+
 
     public List<Contact> getContactsByUserId(Long userId){
         return contactRepository.findByUserId(userId);
