8f1441ba6d9a0615d3c7793b69f6f4b45ed880c5
muhammadanascode <anassohal34343@gmail.com>
2026-01-07 22:45:37 +0500

    feat: created endpoint to register new contact assosiated with particular user

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/controller/ContactController.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/controller/ContactController.java
@@ -1,16 +1,32 @@
 package com.internship.contact_management_backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.internship.contact_management_backend.dto.ContactDto;
+import com.internship.contact_management_backend.entity.Contact;
+import com.internship.contact_management_backend.service.ContactService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contacts")
 public class ContactController {
 
-    @GetMapping
-    public String getContacts() {
-        return "List of contacts";
+    @Autowired
+    private ContactService contactService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ContactDto> createContact(@Valid @RequestBody ContactDto contact){
+
+        //extract the email
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = authentication.getName();
+
+        //create contact
+       Contact savedContact = contactService.createContact(contact.toEntity(),userEmail);
+       return ResponseEntity.ok(savedContact.toDto());
+
     }
 }
