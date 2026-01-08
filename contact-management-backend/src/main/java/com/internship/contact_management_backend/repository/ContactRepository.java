ab4bfc589ac8d1140f9d8b7f0989777ff94a5d3c
muhammadanascode <anassohal34343@gmail.com>
2026-01-08 15:14:51 +0500

    fix: updated the return type

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/repository/ContactRepository.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/repository/ContactRepository.java
@@ -3,11 +3,12 @@ package com.internship.contact_management_backend.repository;
 import com.internship.contact_management_backend.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface ContactRepository extends JpaRepository<Contact,Long> {
     // Find all contacts of a specific user
     List<Contact> findByUserId(Long userId);
 
     // Find contact by id
-    Contact findContactById(Long id);
+    Optional<Contact> findContactById(Long id);
 }
