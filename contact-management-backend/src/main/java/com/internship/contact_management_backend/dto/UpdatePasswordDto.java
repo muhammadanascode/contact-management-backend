ee32d8c3ee660ea5e86f3f582b368eace5dcb718
muhammadanascode <anassohal34343@gmail.com>
2026-01-25 18:12:08 +0500

    feat: created dto to update password

---
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/dto/UpdatePasswordDto.java
@@ -0,0 +1,24 @@
+package com.internship.contact_management_backend.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class UpdatePasswordDto {
+    // Old Password
+    @NotBlank(message = "Old password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String oldPassword;
+
+    // New Password
+    @NotBlank(message = "New password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String newPassword;
+
+    // Confirm new Password
+    @NotBlank(message = "Confirm password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String confirmPassword;
+}
