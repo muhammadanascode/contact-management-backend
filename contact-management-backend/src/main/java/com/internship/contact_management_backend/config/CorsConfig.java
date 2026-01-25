3224fcdaaa99fa20bc99d6207b8d45cb5a46501e
muhammadanascode <anassohal34343@gmail.com>
2026-01-25 21:54:10 +0500

    fix: added field to expose response body

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/config/CorsConfig.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/config/CorsConfig.java
@@ -18,6 +18,7 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of("http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
