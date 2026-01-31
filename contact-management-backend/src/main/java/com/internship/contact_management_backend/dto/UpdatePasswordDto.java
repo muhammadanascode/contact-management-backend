c88e187b7de072c3f1d26af25e7ee4b96ca9efeb
muhammadanascode <anassohal34343@gmail.com>
2026-02-01 00:24:39 +0500

    refactor: removing unused imports

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/dto/UpdatePasswordDto.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/dto/UpdatePasswordDto.java
@@ -1,6 +1,5 @@
 package com.internship.contact_management_backend.dto;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
