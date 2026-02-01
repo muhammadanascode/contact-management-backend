c407de70d86a45244a743f17db459d94e65511e0
muhammadanascode <anassohal34343@gmail.com>
2026-02-01 19:55:35 +0500

    fix: removed un necessary checks

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/dto/UserRegisterDto.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/dto/UserRegisterDto.java
@@ -1,8 +1,8 @@
 package com.internship.contact_management_backend.dto;
 
 import com.internship.contact_management_backend.entity.User;
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -12,24 +12,24 @@ import lombok.Getter;
 @Getter
 public class UserRegisterDto {
     // First name – validation
-    @Column(nullable = false, length = 50)
     @Size(min = 3, max = 50, message = "First name must be between 3 till 50 characters")
+    @NotBlank
     private String firstName;
 
     // Last name – validation
-    @Column(nullable = false, length = 50)
     @Size(min = 3, max = 50, message = "Last name must be between 3 till 50 characters")
+    @NotBlank
     private String lastName;
 
     // Email – unique + valid format
-    @Column(nullable = false, unique = true, length = 100)
     @Email(message = "Email should be valid")
     @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$",message = "Email format is invalid")
+    @NotBlank
     private String email;
 
     // Password – never expose plain text
-    @Column(nullable = false)
     @Size(min = 8, message = "Password must be at least 8 characters")
+    @NotBlank
     private String password;
 
     public User toEntity(){
