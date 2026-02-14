8cb3b5f0c75f14fde20ac7feb76a736a5480f800
muhammadanascode <anassohal34343@gmail.com>
2026-02-14 17:09:44 +0500

    feat: added setter for testing

--- contact-management-backend/src/main/java/com/internship/contact_management_backend/dto/UserLoginDto.java
+++ contact-management-backend/src/main/java/com/internship/contact_management_backend/dto/UserLoginDto.java
@@ -1,8 +1,10 @@
 package com.internship.contact_management_backend.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UserLoginDto {
     private String email;
     private String password;
